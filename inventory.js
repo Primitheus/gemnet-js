4a8eb3f43693cf7ee7c90da37b6799ab5d29603e
Primitheus <45245557+Primitheus@users.noreply.github.com>
2023-03-06 11:18:17 +0000

    Update inventory.js
    Former-commit-id: 1c90e4c5a3a832674adcb298976fc0315159d500

--- inventory.js
+++ inventory.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '***REMOVED***',
+    password: 'toor',
     database: 'rumblefighter'
   });
   
