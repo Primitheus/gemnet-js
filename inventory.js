0141b22edf45a0399bfd8406d86b1ccea9ba3e61
primitheus <primitheus@gmail.com>
2023-04-18 12:36:26 +0100

    updated
    Former-commit-id: 4105c2436e7af9bf6e3e4ff44d387456e8edf1cd

--- inventory.js
+++ inventory.js
@@ -1,10 +1,16 @@
-const { connect } = require('mssql');
 var mysql = require('mysql');
+const chalk = require("chalk");
+
+const success = chalk.green;
+const warning = chalk.red;
+const debug = chalk.magenta;
+const serveract = chalk.cyan;
+
 
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'toor',
+    password: '***REMOVED***',
     database: 'rumblefighter'
   });
   
@@ -23,7 +29,7 @@ function property(uuid) {
     const plength = response.length + 6
     const action = 0x84
 
-    connection.query("SELECT * FROM inventory WHERE UUID= ?", [uuid], function (err, result) {
+    connection.query("SELECT * FROM inventory WHERE uuid= ?", [uuid], function (err, result) {
     if (err) throw err;
     
 
@@ -42,11 +48,12 @@ function property(uuid) {
 
 function buy() {
 
+    const response = Buffer.from([0xbc, 0x3d, 0x21, 0x04, 0x6a, 0x00, 0xd6, 0x01, 0x04, 0x00, 0x00, 0x00]);
+
     const type = 0x30;
     const plength = response.length + 6;
     const action = 0xce;
 
-    const response = Buffer.from([0xbc, 0x3d, 0x21, 0x04, 0x6a, 0x00, 0xd6, 0x01, 0x04, 0x00, 0x00, 0x00]);
 
     return Buffer.concat([
         Buffer.from([(type >> 8) & 0xff, type & 0xff]),
@@ -83,12 +90,14 @@ function cash(uuid) {
     const plength = response.length + 6;
     const action = 0x88;
 
-    connection.query("SELECT astros FROM accounts WHERE UUID= ?", [uuid], function (err, result, fields) {
+    connection.query("SELECT * FROM accounts WHERE uuid= ?", [uuid], function (err, result, fields) {
         if (err) throw err;
         if (result.length > 0) {
             astros = result[0].astros;
+            medals = result[0].medals;
             response.writeUint32LE(uuid, 0);
             response.writeUint32LE(astros, 4);
+            response.writeUint32LE(medals, 8);
         
         }
     })
@@ -102,49 +111,61 @@ function cash(uuid) {
 
 }
 
-function buy_item(itemid, uuid) {
-
-    const response = Buffer.from([0xFF, 0xFF, 0xb7, 0x07, 0xFF, 0x0FF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
-
-
-    const type = 0x31;
-    const plength = response.length + 6;
-    const action = 0x80;
-
-
-    connection.query("SELECT carats FROM accounts WHERE uuid= ?", [uuid], function (err, result, fields) {
-        if(err) throw err;
-        if (result.length > 0)
-        {
-            console.log(result[0]);
-            new_carats = result[0].carats - 1;
-            console.log(new_carats);
-        }
-
-        connection.query("INSERT INTO inventory (itemid, UUID) VALUES ('?', '?')", [itemid, uuid], function (err, result) {
-            if (err) throw err;
-            console.log("User: ", uuid, " has bought item: ", itemid);
-
-        });
-
-        connection.query("UPDATE accounts SET carats = ? WHERE uuid = '?'", [new_carats, uuid], function (err, result) {
-
-        if(err) throw err;
-
-        connection.query("SELECT MAX(ServerID) FROM inventory", function (err, result) {
-            if(err) throw err;
-            serverid = result[0].ServerID;
-
-        });
-
-        if (result.length > 0)
-        {
-            response.writeUInt32LE(serverid, 0);
-            response.writeUInt32LE(new_carats, 4);
-        }
+async function get_property(uuid) {
+    const response = Buffer.alloc(1434, 0);
+  
+    const type = 0x30;
+    const plength = response.length + 6 + 2488;
+    const action = 0x84;
+  
+    console.log("Getting Property From UUID: ", uuid);
+  
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT * FROM inventory WHERE uuid= ?", [uuid], function (err, result, fields) {
+        if (err) reject(err);
+        if (result.length > 0) {
             
-        });
+          console.log(result);
+          console.log(result.length);
+          response.writeUint8(5, 0);
+
+          var x = 2;
+          var y = 6;
+
+          response.writeUInt32LE(result[0].ServerID, x)
+          response.writeUInt32LE(result[0].ItemID, y)
+
+
+            for (let i = 1 ; i < result.length; i++) {
+                console.log("i: ", i);
+                x += 49;
+                y = x + 4;
+                response.writeUInt32LE(result[i].ServerID, x);
+                response.writeUInt32LE(result[i].ItemID, y);
+                console.log("Pos", x, y);
+            }
+
+        }
+        resolve(
+          Buffer.concat([
+            Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+            Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+            Buffer.from([(action + 1) & 0xff, (action + 1) >> 8]),
+            response,
+          ])
+        );
+      });
     });
+  }
+
+
+function get_buddy_list() {
+
+    const response = Buffer.alloc(2, 0)
+
+    const type = 0x10;
+    const plength = response.length + 6;
+    const action = 0xa8;
 
     return Buffer.concat([
         Buffer.from([(type >> 8) & 0xff, type & 0xff]),
@@ -155,4 +176,123 @@ function buy_item(itemid, uuid) {
 
 }
 
-  module.exports = { buy, equip, cash, buy_item}; 
+function gift_from() {
+
+    const response = Buffer.alloc(2, 0)
+
+    const type = 0x30;
+    const plength = response.length + 6;
+    const action = 0x92;
+
+    return Buffer.concat([
+        Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+        Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+        Buffer.from([(action+1) & 0xff, (action+1) >> 8]),
+        response
+    ])
+
+
+}
+
+function gift_to() {
+
+    const response = Buffer.alloc(2, 0)
+
+    const type = 0x30;
+    const plength = response.length + 6;
+    const action = 0x90;
+
+    return Buffer.concat([
+        Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+        Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+        Buffer.from([(action+1) & 0xff, (action+1) >> 8]),
+        response
+    ])
+
+    
+}
+
+function avatar_list() {
+
+    const response = Buffer.alloc(2, 0)
+
+    const type = 0x30;
+    const plength = response.length + 6;
+    const action = 0x88;
+
+    return Buffer.concat([
+        Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+        Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+        Buffer.from([(action+1) & 0xff, (action+1) >> 8]),
+        response
+    ])
+
+
+}
+
+function get_grade() {
+
+    const response = Buffer.alloc(2, 0)
+
+    const type = 0x30;
+    const plength = response.length + 6;
+    const action = 0xa0;
+
+    return Buffer.concat([
+        Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+        Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+        Buffer.from([(action+1) & 0xff, (action+1) >> 8]),
+        response
+    ])
+
+}
+
+
+async function buy_item(itemid, uuid) {
+
+    //const response = Buffer.from([0xFF, 0xFF, 0xb7, 0x07, 0xFF, 0x0FF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    const response = Buffer.alloc(49, 0);
+
+    const type = 0x31;
+    const plength = response.length + 6;
+    const action = 0x80;
+
+
+    return new Promise((resolve, reject) => { connection.query("SELECT carats FROM accounts WHERE uuid= ?", [uuid], function (err, result, fields) {
+        if (err) reject(err);
+        if (result.length > 0)
+        {
+
+            console.log(result[0]);
+            new_carats = result[0].carats - 1;
+            console.log(new_carats);
+
+            if (new_carats >= 0) { 
+                connection.query("UPDATE accounts SET carats = ? WHERE uuid = '?'", [5000000, uuid], function (err, result) {
+                
+                });
+
+                connection.query("INSERT INTO inventory (ItemID, UUID) VALUES ('?', '?')", [itemid, uuid], function (err, result) {
+                    console.log(success("User:", uuid, "has bought item:", itemid));
+                    console.log(result);
+                    serverid = result.insertId;
+                    response.writeUInt32LE(serverid, 0);
+                    response.writeUint32LE(5000000, 4);
+
+                    resolve(
+                        Buffer.concat([
+                          Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+                          Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+                          Buffer.from([(action + 1) & 0xff, (action + 1) >> 8]),
+                          response,
+                        ])
+                      );
+                    
+                });
+            }
+        }
+    });
+});
+}
+
+  module.exports = { buy, equip, cash, buy_item, get_property, gift_from, gift_to, get_grade, avatar_list, get_buddy_list }; 
--- login.js
+++ login.js
@@ -13,7 +13,7 @@ var forum_name;
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'toor',
+    password: '***REMOVED***',
     database: 'rumblefighter'
   });
   
@@ -37,8 +37,89 @@ function version_check() {
     return response
 }
 
-function credential_check(email, password) {
+async function credential_check(email, password) {
 
+  const response = Buffer.alloc(320, 0);
+  const type = 0x10;
+  const plength = response.length + 6;
+  const action = 0x84;
+    
+      return new Promise((resolve, reject) => {connection.query("SELECT * FROM accounts WHERE email=? AND password=?", [email, password], function (err, result, fields) {
+        if (err) reject(err);
+          if (result.length > 0)
+          {
+            if (result[0].has_character == 1)
+            {
+              console.log(result[0].email);
+              console.log("Login Successful");
+              uuid = result[0].uuid;
+              exp = result[0].exp;
+              carats = result[0].carats;
+              astros = result[0].astros;
+              userign = result[0].username;
+              GUID = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
+              token = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234";
+              forum_name = result[0].forum_name;
+    
+              response.writeUint32LE(uuid, 0);
+              response.write(userign, 4);
+              response.writeUint32LE(exp, 28); // Exp
+              response.writeUint32LE(carats, 32);
+              response.write(GUID, 90);
+              response.write(token, 191);
+              response.write(forum_name, 256);
+              
+              //byteUsername = ascii_to_hex(userign);
+              
+              resolve(
+                Buffer.concat([
+                  Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+                  Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+                  Buffer.from([(action + 1) & 0xff, (action + 1) >> 8]),
+                  response,
+                ])
+              );
+    
+          } else {
+            console.log("Login Successful, No Character")
+
+            const response = Buffer.alloc(520, 0);
+            const type = 528;
+            const action = HEADER.ACTION.CRED_CHECK;
+
+            resolve(
+              Buffer.concat([
+                Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+                Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+                Buffer.from([(action + 1) & 0xff, (action + 1) >> 8]),
+                response,
+              ])
+            );
+
+          }  
+ 
+          } else {
+            console.log("User doesn't exist");
+
+            const response = Buffer.alloc(514, 0);
+            const type = 528;
+            const plength = response.length + 6;
+            const action = 0xbe0285 - 1;
+            //const temp = 0x02be;
+
+            response.write("¾Please check your Email or Password and try again.")
+
+            resolve(
+              Buffer.concat([
+                Buffer.from([(type >> 8) & 0xff, type & 0xff]),
+                Buffer.from([(plength >> 8) & 0xff, plength & 0xff]),
+                Buffer.from([(action + 1) & 0xff, (action + 1) >> 8]),
+                response,
+              ])
+            );
+          }           
+          })
+        });
 }
 
 function server_time() {
